0141c0ac2c3d40d36c22ca13d994fcadfe3ef678
zannat20040 <shanzidadisha1234@gmail.com>
2025-04-02 21:59:21 +0600

    headline component add

---
+++ src/_components/shared/Headline.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Headline() {
+  return (
+    <div className="flex gap-3 items-center container mx-auto  my-12 px-5 lg:px-8 ">
+      <div className="bg-pale-red h-9 w-[10px] "></div>
+      <p className="text-2xl font-bold ">Flash Sale</p>
+    </div>
+  );
+}
