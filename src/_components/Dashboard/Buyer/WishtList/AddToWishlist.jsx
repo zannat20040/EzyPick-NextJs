fa27d3b27ac2d69b81a1c299cc636727d3b5dbfb
zannat20040 <shanzidadisha1234@gmail.com>
2025-04-24 18:58:16 +0600

    wishlist error

--- src/_components/Dashboard/Buyer/WishtList/AddToWishlist.jsx
+++ src/_components/Dashboard/Buyer/WishtList/AddToWishlist.jsx
@@ -1,34 +1,43 @@
 "use client";
-
 import React from "react";
 import { IoHeartSharp } from "react-icons/io5";
 import axiosInstance from "@/utils/axiosInstance";
 import toast from "react-hot-toast";
 import { useAuth } from "@/Context/AuthContext";
+import getUserByEmail from "@/utils/getUserByEmail";
 
 export default function AddToWishlist({ productId }) {
   const { user } = useAuth();
 
-  const HandleAddToWishlist = async () => {
-    if (!user?.email) {
-      return toast.error("Please log in to add to wishlist.");
-    }
-
-    try {
-      const res = await axiosInstance.post("/api/user-cart/wishlist/add", {
-        email: user.email,
-        productId,
-      });
-
-      if (res.data) {
-        toast.success("Added to wishlist!");
+  const HandleAddToWishlist = () => {
+    // Run async logic inside this
+    (async () => {
+      if (!user?.email) {
+        return toast.error("Please log in to add to wishlist.");
       }
-    } catch (err) {
-      console.log(err);
-      toast.error(
-        err.response?.data.message || err.message || "Failed to add to wishlist"
-      );
-    }
+
+      const userData = await getUserByEmail(user.email);
+      if (!userData) return toast.error("Failed to get user data.");
+
+      try {
+        const res = await axiosInstance.post("/api/user-cart/wishlist/add", {
+          email: user.email,
+          username: userData.name,
+          productId,
+        });
+
+        if (res.data) {
+          toast.success("Added to wishlist!");
+        }
+      } catch (err) {
+        console.error(err);
+        toast.error(
+          err.response?.data?.message ||
+            err.message ||
+            "Failed to add to wishlist"
+        );
+      }
+    })();
   };
 
   return (
