2ad7e5c16b59037adbd74a9c8df1e2af296a0a89
zannat20040 <shanzidadisha1234@gmail.com>
2025-04-27 13:32:51 +0600

    order list update

--- src/_components/Dashboard/Buyer/order/OrderList.jsx
+++ src/_components/Dashboard/Buyer/order/OrderList.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -8,8 +6,7 @@ export default function OrderList({ orders, email }) {
   return (
     <ul className="list bg-base-100 container px-5 lg:px-8 mx-auto">
       {orders.map((order) => (
-        <li key={order._id} className="list-row px-0 border-b last:border-b-0 py-4">
-          <div className="flex flex-col w-full">
+        <li key={order._id} className="list-row px-0 border-b last:border-b-0 py-4 flex flex-col">
             <div className="flex justify-between items-center">
               <div className="text-sm text-gray-500 font-medium">
                 Order ID: <span className="text-black">{order._id.slice(-6).toUpperCase()}</span>
@@ -19,7 +16,7 @@ export default function OrderList({ orders, email }) {
               </div>
             </div>
 
-            <div className="flex gap-5 flex-wrap mt-3">
+            <div className="flex gap-5 flex-wrap mt-3 ">
               {order.items.map((item, idx) => (
                 <div key={idx} className="flex items-center gap-4 bg-gray-50 p-3 rounded-lg w-full sm:w-auto">
                   <Image
@@ -45,15 +42,7 @@ export default function OrderList({ orders, email }) {
               ))}
             </div>
 
-            <div className="text-right mt-4">
-              <Link
-                href={`/user/orderdetails/${order._id}`}
-                className="btn btn-sm bg-pale-red text-white rounded font-semibold uppercase"
-              >
-                View Details
-              </Link>
-            </div>
-          </div>
+           
         </li>
       ))}
     </ul>
